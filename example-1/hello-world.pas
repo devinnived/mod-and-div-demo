1ad7191db9c2eb4297dc8103d6fc1f4bbc073236
devinnived <davendra.naraine@nived.io>
2024-09-22 11:02:10 -0400

    add examples of variable types

--- example-1/hello-world.pas
+++ example-1/hello-world.pas
@@ -1,6 +1,22 @@
 program HelloWord;
 
+(*var, short for variables are used to store pieces of information.*)
+var
+position: integer; (*1 2 3 10 100*)
+grade: char; (*A B C D F*)
+pass: boolean; (*true false*)
+name: string; (*Davendra 'Hello welcome.'*)
+
 (* Program block starts *)
 begin
-  writeln ('Hello world.');
+  position := 10;
+  grade := 'A';
+  pass := true;
+  name := 'Davendra';
+
+  writeln (position);
+  writeln (grade);
+  writeln (pass);
+  writeln (name);
+  writeln (10/3);
 end.
